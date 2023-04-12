995691fd787312af0db6afbab3c7f8ec3aa5d844
Kord Boniadi <kordbon1@gmail.com>
2023-04-11 19:02:28 -0700

    themis bytes read and decoded rana side

--- Rana/services/VIDEO_SERVICE.cpp
+++ Rana/services/VIDEO_SERVICE.cpp
@@ -1,8 +1,8 @@
 #include "VIDEO_SERVICE.h"
 
 SDL_Texture *texture = NULL;
-int width = 1920;
-int height = 816;
+int width = 800;
+int height = 600;
 
 bool Initialize_VIDEO_SERVICE(VIDEO_SERVICE *video, KCONTEXT *ctx) {
 	video->ctx = ctx;
@@ -14,7 +14,7 @@ bool Initialize_VIDEO_SERVICE(VIDEO_SERVICE *video, KCONTEXT *ctx) {
 	DEBUG(("H.264 Decoder API v%d.%d\n", broadwayGetMajorVersion(),
 		   broadwayGetMinorVersion()));
 
-	broadwayInit(&video->decoder, 1, 0, 0, 0);
+	broadwayInit(&video->decoder, 0, 0, 0, 0);
 	video->byteStrmStart = broadwayCreateStream(&video->decoder, MAX_NAL_SIZE);
 
 	// Initialize SDL
@@ -32,8 +32,7 @@ bool Initialize_VIDEO_SERVICE(VIDEO_SERVICE *video, KCONTEXT *ctx) {
 
 	// Create an SDL texture
 	texture = SDL_CreateTexture(video->renderer, SDL_PIXELFORMAT_IYUV,
-								SDL_TEXTUREACCESS_STREAMING, width,
-								height);
+								SDL_TEXTUREACCESS_STREAMING, width, height);
 	return true;
 }
 
@@ -78,14 +77,15 @@ extern void broadwayOnPictureDecoded(u8 *buffer, u32 width, u32 height) {
 	yuv_to_pixels(buffer, (width * height * 3) / 2);
 }
 
-extern void broadwayOnHeadersDecoded() { printf("header decoded"); }
+extern void broadwayOnHeadersDecoded() { printf("header decoded\n"); }
 
 void Main_TCP_Loop_VIDEO_SERVICE(void *arg) {
 	VIDEO_SERVICE *video = (VIDEO_SERVICE *)arg;
 	int size;
 	if (Receive_CLIENT(video->c, (char *)&size, sizeof(int)) &&
 		Receive_CLIENT(video->c, video->nal_buffer, size)) {
-		video->decoder.decInput.pStream = (u8*)video->nal_buffer;
+		video->decoder.streamStop = (u8 *)video->nal_buffer + size;
+		video->decoder.decInput.pStream = (u8 *)video->nal_buffer;
 		video->decoder.decInput.dataLen = size;
 
 		// Clear the renderer
@@ -94,18 +94,16 @@ void Main_TCP_Loop_VIDEO_SERVICE(void *arg) {
 		SDL_RenderCopy(video->renderer, texture, NULL, NULL);
 		// Render the texture to the screen
 		SDL_RenderPresent(video->renderer);
+		cout << "before" << endl;
 
-		if (!video->stop) {
-			// playStream(&dec, &dec.broadwayStream);
+		u32 i = 0;
+		do {
 			u8 *start = video->decoder.decInput.pStream;
 			u32 ret = broadwayDecode(&video->decoder);
-			printf("Decoded Unit #%d, Size: %d, Result: %d\n", video->counter++,
+			printf("Decoded Unit #%d, Size: %d, Result: %d\n", i++,
 				   (video->decoder.decInput.pStream - start), ret);
-		}
+		} while (video->decoder.decInput.dataLen > 0);
 
-		if (video->decoder.decInput.dataLen <= 0) {
-			video->stop = 1;
-		}
 		cout << "received video bytes " << size << endl;
 	}
 }
@@ -134,7 +132,7 @@ bool Connect_VIDEO_SERVICE(VIDEO_SERVICE *video, CLIENT *video_init,
 	ASSERT_E_R(Receive_CLIENT(video_init, video->nal_buffer, size),
 			   "Failed to receive headers", video->ctx);
 	video->main_loop_running = true;
-	
+
 	emscripten_set_main_loop_arg(
 		[](void *arg) { Main_TCP_Loop_VIDEO_SERVICE(arg); }, video, 30, 1);
 	// video->main_loop = new thread(Main_TCP_Loop_VIDEO_SERVICE, video);
