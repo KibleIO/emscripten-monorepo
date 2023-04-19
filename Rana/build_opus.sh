a7d0c9c6888453ad501a2195d37272eb7cd4d9ba
Kord Boniadi <kordbon1@gmail.com>
2023-04-18 17:32:56 -0700

    lock opus to v1.3.1

--- Rana/build_opus.sh
+++ Rana/build_opus.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 rm -rf opus/
-git clone https://github.com/xiph/opus.git
+
+wget https://github.com/xiph/opus/archive/refs/tags/v1.3.1.tar.gz
+
+tar -xzf v1.3.1.tar.gz
+rm v1.3.1.tar.gz
+mv opus-1.3.1 opus
+
 cd opus
 mkdir build
 cd build
+
 ../autogen.sh
 emconfigure ../configure --disable-extra-programs --disable-rtcd --disable-intrinsics
 emmake make
