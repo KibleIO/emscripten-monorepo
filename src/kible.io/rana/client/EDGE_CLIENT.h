bfc8aad23aef8f8c3a8285ffd2567c15745c63be
j0ker <ktb92677@yahoo.com>
2023-05-11 22:03:30 -0700

    edge stuff

---
+++ src/kible.io/rana/client/EDGE_CLIENT.h
@@ -0,0 +1,9 @@
+#ifndef _EDGE_CLIENT_H_
+#define _EDGE_CLIENT_H_
+
+#include <string>
+#include "../pb/EDGE_CLIENT.h"
+
+bool Themis_EDGE_CLIENT(std::string, std::string*);
+
+#endif
--- src/kible.io/rana/pb/EDGE_CLIENT.cpp
+++ src/kible.io/rana/pb/EDGE_CLIENT.cpp
@@ -4,3 +4,7 @@ HTTP_Protobuf_Init_Delete(kible.edge, EDGE, /api)
 
 HTTP_Protobuf_Endpoint_Definition(EDGE, Themis, kible::edge::ThemisRequest,
 	kible::edge::ThemisResponse)
+HTTP_Protobuf_Endpoint_Definition(EDGE, Kasm, kible::edge::KasmRequest,
+	kible::edge::KasmResponse)
+HTTP_Protobuf_Endpoint_Definition(EDGE, Login, kible::edge::LoginRequest,
+	kible::edge::LoginResponse)
